87e8d86493cbb23f424a05c346ab4291c1eb24e7
Mkiołaj Mucha <mikolaj.mucha1@gmail.com>
2022-11-24 17:02:28 +0100

    massage1

--- kodilla-intro/src/test/java/kodilla/FirstCalss.java
+++ kodilla-intro/src/test/java/kodilla/FirstCalss.java
@@ -1,8 +1,29 @@
 package kodilla;
 
-class FirstClass {                           // [1]
-    public static void main(String[] args) {         // [2]
-        System.out.println("Hello from kodilla.FirstClass!"); // [3]
-    }
-}
+public class FirstCalss{
+    public static void main(String[] args){
+        Notebook heavyNotebook = new Notebook(2000,800 );
+        System.out.println("heavyNotebook weight: " +heavyNotebook.weight);
+        System.out.println("heavyNotebook price " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook(1200, 300);
+        System.out.println("oldNotebook weight: " + oldNotebook.weight);
+        System.out.println("oldNotebook price " + oldNotebook.price);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price );
+        oldNotebook.checkPrice();
+
+        Notebook notebook = new Notebook(600,1000 );
+        System.out.println("notebook weight: " + notebook.weight);
+        System.out.println("notebook price " + notebook.price);
+        notebook.checkPrice();
+
+
+
+
+
+        }
+
+   }
+
 
---
+++ kodilla-intro/src/test/java/kodilla/Notebook.java
@@ -0,0 +1,25 @@
+package kodilla;
+
+public class Notebook {
+    int weight;
+    int price;
+
+    public Notebook(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+    public  void checkPrice(){
+        if (this.price < 900) {
+            System.out.println("This notebook is cheap");
+        }else {
+            System.out.println("This notebook is quite expensivegit");
+        }
+    }
+
+
+
+    }
+
+
+
+
