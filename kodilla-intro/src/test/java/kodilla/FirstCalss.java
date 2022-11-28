dfa24b9bc03a6f7cc7b16cc45421dba7776987fe
Mkiołaj Mucha <mikolaj.mucha1@gmail.com>
2022-11-28 19:16:42 +0100

    massage3

--- kodilla-intro/src/test/java/kodilla/FirstCalss.java
+++ kodilla-intro/src/test/java/kodilla/FirstCalss.java
@@ -2,21 +2,37 @@ package kodilla;
 
 public class FirstCalss{
     public static void main(String[] args){
-        Notebook heavyNotebook = new Notebook(2000,800 );
-        System.out.println("heavyNotebook weight: " +heavyNotebook.weight);
-        System.out.println("heavyNotebook price " + heavyNotebook.price);
+        Notebook heavyNotebook = new Notebook(2000,1500, 2015 );
+        System.out.println("heavyNotebook");
+        System.out.println(" weight: " +heavyNotebook.weight);
+        System.out.println(" price " + heavyNotebook.price);
+        System.out.println(" year " + heavyNotebook.year);
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkQuality();
+        System.out.println("  ");
 
-        Notebook oldNotebook = new Notebook(1200, 300);
-        System.out.println("oldNotebook weight: " + oldNotebook.weight);
-        System.out.println("oldNotebook price " + oldNotebook.price);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price );
+
+
+        Notebook oldNotebook = new Notebook(1200, 2200, 2000);
+        System.out.println("oldNotebook");
+        System.out.println(" weight: " + oldNotebook.weight);
+        System.out.println(" price " + oldNotebook.price);
+        System.out.println(" year " + oldNotebook.year);
         oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkQuality();
+        System.out.println("  ");
 
-        Notebook notebook = new Notebook(600,1000 );
-        System.out.println("notebook weight: " + notebook.weight);
-        System.out.println("notebook price " + notebook.price);
+        Notebook notebook = new Notebook(600,1000, 2022 );
+        System.out.println("Notebook");
+        System.out.println(" weight: " + notebook.weight);
+        System.out.println(" price " + notebook.price);
+        System.out.println(" year " + notebook.year);
         notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkQuality();
+
 
 
 
--- kodilla-intro/src/test/java/kodilla/Notebook.java
+++ kodilla-intro/src/test/java/kodilla/Notebook.java
@@ -4,21 +4,58 @@ public class Notebook {
     int weight;
     int price;
 
-    public Notebook(int weight, int price) {
+    int year;
+
+    public Notebook(int weight, int price, int year) {
         this.weight = weight;
         this.price = price;
+        this.year = year;
     }
     public  void checkPrice(){
-        if (this.price < 900) {
-            System.out.println("This notebook is cheap");
-        }else {
-            System.out.println("This notebook is quite expensivegit");
+        if (this.price < 600) {
+            System.out.println("This notebook is  very cheap.");
+        }else if (this.price  > 600 || this.price < 1000) {
+            System.out.println("The price is good.");
+        } else if (this.price > 1000){
+            System.out.println("This notebook is expensive.");
+        }
+    }
+    public void checkWeight() {
+        if (this.weight < 800) {
+            System.out.println("This notebook is light");
+        } else if (this.weight > 800 || this.weight < 1500) {
+            System.out.println("This notebook is medium weight");
+        } else if (this.weight > 1500) {
+            System.out.println("This notebook is heavy");
         }
     }
 
+    public void checkQuality(){
+        if (this.price > 2000 || this.year < 2010) {
+            System.out.println("This notebook is inviable.");
+        } else if (this.price > 1200 || this.year > 2010) {
+            System.out.println("This notebook is viable.");
+        } else if (this.price <1200 || this.year > 2018) {
+            System.out.println("This notebook is the most viable");
+        }else {
+            System.out.println("This notebook is ok");
+        }
+
 
 
     }
+
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
