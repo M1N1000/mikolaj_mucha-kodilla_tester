c34a616336f3b080ef42200dfc94b9ebfc38d230
Mkiołaj Mucha <mikolaj.mucha1@gmail.com>
2022-12-01 19:14:12 +0100

    Random

--- kodilla-intro/src/test/java/kodilla/RandomNumbers.java
+++ kodilla-intro/src/test/java/kodilla/RandomNumbers.java
@@ -5,8 +5,8 @@ import java.util.Random;
 public class RandomNumbers {
 
     public static void main(String[] args) {
-
-
+        int min = 30;
+        int max = 0;
         int end = 5000;
         Random numbers = new Random();
 //        int los = numbers.nextInt(31);
@@ -16,12 +16,24 @@ public class RandomNumbers {
         for (int i = 0; i <= end; i++) {
             if (suma < end) {
                 int los = numbers.nextInt(31);
+
                 //losowanie jest tutaj zeby z kazdym nowym obrotem była nowa liczba
                 suma = suma + los;
                 System.out.println(suma);
+                if (min > los){
+                    min = los;
+                    System.out.println("to jest min: " +min);
+                } else if (max<los) {
+                    max = los;
+                    System.out.println("to jest max: " + max);
 
-
+                }
             }
+
+
+
+
+
         }
     }
 }
