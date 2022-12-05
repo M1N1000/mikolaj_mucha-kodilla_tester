3ec75ed45fb625ff77013ac3dcdba4d3ec831136
Mkiołaj Mucha <mikolaj.mucha1@gmail.com>
2022-12-05 17:05:55 +0100

    Switch

---
+++ kodilla-intro/src/test/java/kodilla/Switch.java
@@ -0,0 +1,9 @@
+package kodilla;
+
+public class Switch {
+        public static void main(String[] args){
+        String color = UserDialogs.getColor();
+        System.out.println("Hi " + color + " is your color");
+    }
+}
+
--- kodilla-intro/src/test/java/kodilla/UserDialogs.java
+++ kodilla-intro/src/test/java/kodilla/UserDialogs.java
@@ -1,15 +1,33 @@
 package kodilla;
 import java.util.Scanner;
 public class UserDialogs {
-    public static String getUsername() {
+    public static String getColor() {
         Scanner scanner = new Scanner(System.in);                 // [1]
         while (true) {                                            // [2]
-            System.out.println("Enter your name:");                // [3]
-            String name = scanner.nextLine().trim();               // [4]
-            if (name.length() >= 2) {                              // [5]
-                return name;                                        // [6]
+            System.out.println("Enter your First color letter:");                // [3]
+            String firstColorLetter = scanner.nextLine().trim().toUpperCase();
+            switch (firstColorLetter) {
+                case "R":
+                    return "Red";
+                case "B":
+                    return "Blue";
+                case "O":
+                    return "Orange";
+                case "Y":
+                    return "Yellow";
+                case "W":
+                    return "White";
+                case "P":
+                    return "Pink";
+                case "G":
+                    return "Grey";
+
+                default:
+                    System.out.println("Wrong letter. Try again.");// [4]
+                    // [6]
             }
-            System.out.println("Name is too short. Try again.");
+
         }
     }
 }
+
