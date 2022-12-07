1d867c0924295ac88cf9438adbcf43ed79028518
Mkiołaj Mucha <mikolaj.mucha1@gmail.com>
2022-12-07 18:05:56 +0100

    Calculator

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_asserition/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.basic_asserition;
+
+public class Calculator {
+
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int potega(int a) {
+        return a * a;
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_asserition/resultChecker.java
@@ -0,0 +1,8 @@
+package com.kodilla.basic_asserition;
+
+public class resultChecker {
+    public static boolean assertEquals(int expected, int actual) {
+        return expected == actual;
+    }
+
+}
