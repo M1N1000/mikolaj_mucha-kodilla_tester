28da9f3f7f9aadd04a11c4cc91eef6c5a9905e71
Mkiołaj Mucha <mikolaj.mucha1@gmail.com>
2023-03-13 16:35:16 +0100

    9.1

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -1,4 +1,10 @@
 package com.kodilla.spring.basic.dependency_injection.homework;
 
 public class Application {
+    public static void main(String[] args) {
+        DeliveryService deliveryService = new UpsDeliverService();
+        NotificationService notificationService = new StatusNotificationService();
+        ShippingCenter shippingCenter = new ShippingCenter(deliveryService,notificationService);
+        shippingCenter.sendPackage("Hill Street 11, New York", 18.2);
+    }
 }
